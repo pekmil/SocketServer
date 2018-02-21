b2e9f32fb73ea22fceab53abcbe9fdfdabd2f047
Kacsa <horvath@dcs.uni-pannon.hu>
2018-02-21 11:12:59 +0100

    Add program

---
+++ SocketServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SocketServer;
+
+namespace Program
+{
+    class ServiceProgram
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int port = 50000;
+                AsyncService service = new AsyncService(port);
+                service.Run();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SocketServer/ServerService.cs
@@ -0,0 +1,102 @@
+﻿using Communication;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace SocketServer
+{
+    public class AsyncService
+    {
+        private IPAddress ipAddress;
+        private int port;
+        public AsyncService(int port)
+        {
+            this.port = port;
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(hostName);
+            this.ipAddress = null;
+            for (int i = 0; i < ipHostInfo.AddressList.Length; ++i)
+            {
+                if (ipHostInfo.AddressList[i].AddressFamily ==
+                  AddressFamily.InterNetwork)
+                {
+                    this.ipAddress = ipHostInfo.AddressList[i];
+                    break;
+                }
+            }
+            if (this.ipAddress == null)
+                throw new Exception("No IPv4 address for server");
+        }
+        public async void Run()
+        {
+            TcpListener listener = new TcpListener(this.ipAddress, this.port);
+            listener.Start();
+            Console.Write("Test socket service is now running");
+            Console.WriteLine(" " + this.ipAddress + " on port " + this.port);
+            Console.WriteLine("Hit <enter> to stop service\n");
+            while (true)
+            {
+                try
+                {
+                    TcpClient tcpClient = await listener.AcceptTcpClientAsync();
+                    Task t = Process(tcpClient);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        private async Task Process(TcpClient tcpClient)
+        {
+            string clientEndPoint = tcpClient.Client.RemoteEndPoint.ToString();
+            Console.WriteLine("Received connection request from " + clientEndPoint);
+            try
+            {
+                NetworkStream networkStream = tcpClient.GetStream();
+                StreamReader reader = new StreamReader(networkStream);
+                StreamWriter writer = new StreamWriter(networkStream);
+                writer.AutoFlush = true;
+
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+                while (true)
+                {
+                    string requestStr = await reader.ReadLineAsync();
+                    if (requestStr != null)
+                    {
+                        CommObject request = serializer.Deserialize<CommObject>(requestStr);
+                        Console.WriteLine("Received service request: " + request);
+                        CommObject response = Response(request);
+                        Console.WriteLine("Computed response is: " + response + "\n");
+                        await writer.WriteLineAsync(serializer.Serialize(response));
+                    }
+                    else
+                    {
+                        Console.WriteLine(requestStr == null);
+                        break; // Client closed connection
+                    }
+                }
+                tcpClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                if (tcpClient.Connected)
+                    tcpClient.Close();
+            }
+        }
+        private static CommObject Response(CommObject request)
+        {
+            CommObject response = new CommObject(request.Message + " (Server)");
+            return response;
+        }
+    }
+}
